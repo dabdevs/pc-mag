52c4f6e7d9171968a6a246cdf2260a82df7eefe7
Alain Jean <ajean@sugit.com.ar>
2023-12-14 19:13:37 -0300

    Validate user creation data

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,39 +2,38 @@ const User = require('../models/User')
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
+const { validationResult } = require('express-validator');
 
-module.exports.signupGet = (req, res) => {
-    res.send('signup get')
-}
+module.exports.register = async (req, res) => {
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 
-module.exports.signupPost = async (req, res) => {
-    const {email, password, name, role} = req.body
+    const { email, password, name, role } = req.body
 
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
-        await User.create({ email, password: hashedPassword, name, role})
+        await User.create({ email, password: hashedPassword, name, role })
         res.status(201).send('User registered successfully.');
     } catch (err) {
         console.log(err)
-        res.status(500).json({message: 'Internal server error'})
+        res.status(500).json({ message: 'Internal server error' })
     }
 }
 
-module.exports.loginGet = (req, res) => {
-    res.send('login get')
-}
-
-module.exports.loginPost = async (req, res) => {
+module.exports.login = async (req, res) => {
     const { email, password } = req.body;
 
     try {
         const user = await User.findOne({ email });
         const matched = await bcrypt.compare(password, user.password)
-        
+
         if (!user || !matched) {
             return res.status(401).send('Invalid email or password.');
         }
-        
+
         const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
         res.json({ token });
     } catch (error) {
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,12 +1,25 @@
 const { Router } = require('express')
 const authController = require('../controllers/auth.controller')
 const router = Router()
+const { body } = require('express-validator');
+const User = require('../models/User')
 
+// Validation middleware for user creation
+const validateUser = [
+    body('email').isEmail().withMessage('Invalid email address').custom(async (value) => {
+        // Check if the email already exists in the database
+        const user = await User.findOne({ email: value });
+        if (user) {
+            return Promise.reject('Email is already in use');
+        }
+        return true;
+    }),
+    body('password').trim().isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
+    body('name').trim().isLength({ max: 50 }).withMessage('Name must be at most 50 characters'),
+];
 
-router.get('/signup', authController.signupGet)
-router.post('/signup', authController.signupPost)
-router.get('/login', authController.loginGet)
-router.post('/login', authController.loginPost)
+router.post('/register', validateUser, authController.register)
+router.post('/login', authController.login)
 router.post('/logout', authController.logout)
 
 module.exports = router
