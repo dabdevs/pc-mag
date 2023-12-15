4f9985f72a7bf529e6d887a6f8f00786f1c8dd4e
Alain Jean <ajean@sugit.com.ar>
2023-12-15 16:11:51 -0300

    Products option button

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -2,14 +2,21 @@ import React from 'react'
 import { Navigate } from 'react-router-dom'
 import { useAuthContext } from '../context/AuthContext'
 import Layout from '../components/Layout'
+import { SiProducthunt } from "react-icons/si";
 
 export default function Dashboard() {
-  const { authUser, token } = useAuthContext()
+  const { token } = useAuthContext()
 
   return !token ? <Navigate to={'/login'} /> : (
     <Layout classes={'p-3'}>
-      <section className='card col-sm-8 mx-auto my-5 p-3'>
-        <p>Hello {authUser?.name}</p>
+      <section className='col-sm-8 mx-auto my-5 p-3'>
+        <div className='row'>
+            <div onClick={() => window.location.href = '/products'} className='card col-sm-3 p-4 d-flex flex-column align-items-center' role='button'>
+              <SiProducthunt style={{ fontSize: '100px' }} />
+              
+              <h6 className='mt-3'>Products</h6>
+            </div>
+        </div>
       </section>
     </Layout>
   )
