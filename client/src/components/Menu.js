2a66a07893151f89c33c61c55bb31059127af352
Alain Jean <ajean@sugit.com.ar>
2024-03-20 21:46:26 -0300

    fix menu

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,14 +1,9 @@
 import React, { useEffect } from 'react'
-import { useComputersContext } from '../context/ComputersContext'
 import { Link, useParams } from 'react-router-dom';
-import parse from 'html-react-parser';
 
 export default function Menu() {
-    const { setCategory } = useComputersContext()
     const data = JSON.parse(localStorage.getItem('computerFormData'))
     const { category } = useParams()
-
-    useEffect(() => setCategory(category), [category])
 
     return (
         <nav className="card bg-white navbar mb-4 navbar-expand-lg w-100 navbar-light bg-light mb-2">
@@ -18,11 +13,10 @@ export default function Menu() {
                         data?.categories.map(cat => (
                             <li className="nav-item mx-3 p-0" key={cat.name}>
                                 <Link 
-                                    className={cat.name.toLowerCase() === category || typeof (category) === 'undefined' && cat.name === 'Computers' ? 'text-danger nav-link text-decoration-underline' : 'nav-link'} 
+                                    className={`nav-link ${cat.name.toLowerCase() === category || typeof (category) === 'undefined' && (cat.name === 'Computers') ? 'text-decoration-underline text-danger' : 'nav-link'}`} 
                                     aria-current="page" 
                                     to={`/${cat.name.toLowerCase()}`}
                                 > 
-                                    {parse(cat.icon)}
                                     {cat.name}
                                 </Link>
                             </li>
--- client/src/views/admin/DashboardMenu.js
+++ client/src/views/admin/DashboardMenu.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import parse from 'html-react-parser';
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function DashboardMenu() {
-    const { category } = useParams()
     const data = JSON.parse(localStorage.getItem('computerFormData'))
+    const path = window.location.pathname.replace('/admin/','')
 
     return (
         <div className='d-flex justify-content-between'>
@@ -13,10 +12,9 @@ export default function DashboardMenu() {
                     <Link 
                         key={cat.name} 
                         to={`${cat.name.toLowerCase()}`} 
-                        className={`card text-decoration-none link-dark col-sm-2 p-2 d-flex flex-column align-items-center ${category === cat.name.toLowerCase() ? 'border-3 border-dark' : ''}`}
+                        className={`card text-decoration-none link-dark col-sm-2 p-2 d-flex flex-column align-items-center ${path === cat.name.toLowerCase() ? 'border-3 border-dark' : ''}`}
                     >
                         <h6 className='mt-3 text-center'>
-                            <span style={{ fontSize: '50px' }}>{parse(cat.icon)}</span> <br />
                             {cat.name}
                         </h6>
                     </Link>
